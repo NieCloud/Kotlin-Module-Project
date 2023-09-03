60874a4ca572651b5d038176000f9be072173a8a
NieCloud <firdem@yandex.ru>
2023-09-03 12:38:45 +0300

    1. Корректная нумерация в случае если есть заметки/архивы, и если их нет. В случае некорректного выбора - отображается ошибка. 2. При выборе заметки/архива, высвечивается список всех заметок/архивов 3. Все ошибки корректно обрабатываются

--- src/main/kotlin/SelectArchiveScreen.kt
+++ src/main/kotlin/SelectArchiveScreen.kt
@@ -1,6 +1,6 @@
 class SelectArchiveScreen(private val archives: MutableList<Archive>) : Screen() {
 
-    private val MENU_WHEN_ARHCHIVES_ARE_EMPTY = "Введите\n2. Создать новый архив\n4. Выйти из программы"
+    private val MENU_WHEN_ARHCHIVES_ARE_EMPTY = "Введите\n1. Создать новый архив\n2. Выйти из программы"
     private val MENU_WHEN_ARCHIEVES_ARE_FILLED = "Введите\n1. Выбрать архив\n2. Создать новый архив\n3. Посмотреть список всех архивов\n4. Выйти из программы"
 
 
@@ -8,33 +8,54 @@ class SelectArchiveScreen(private val archives: MutableList<Archive>) : Screen()
 
         val input = WorkWithUserInput.getUserInput(emptyMenu = MENU_WHEN_ARHCHIVES_ARE_EMPTY, filledMenu = MENU_WHEN_ARCHIEVES_ARE_FILLED, isEmptyDataSet = archives.isEmpty())
 
-        when (input.toIntOrNull()) {
-            1 -> {
-                val archiveName = WorkWithUserInput.getUserInput("Введите название архива:")
-                val selectedArchive = archives.find { it.name == archiveName }
-                if (selectedArchive != null) {
-                    println("Архив выбран!")
-                    nextScreen = SelectNoteScreen(selectedArchive)
-                    nextScreen?.previousScreen = this
-                    nextScreen?.show()
-                } else {
-                    WorkWithUserInput.showError("Архив '$archiveName' не существует.")
+        if (archives.isEmpty()) {
+            when (input.toIntOrNull()) {
+                1 -> goToNewArchiveCreation()
+                2 -> exit()
+                else -> checkArchievesForInput(input)
+            }
+        } else {
+            when (input.toIntOrNull()) {
+                1 -> {
+                    printArchievesNames()
+                    chooseArchives()
+                }
+                2 -> goToNewArchiveCreation()
+                3 -> {
+                    printArchievesNames()
                     show()
                 }
+                4 -> exit()
+                else -> checkArchievesForInput(input)
             }
-            2 -> {
-                nextScreen = CreateArchiveScreen(archives)
-                nextScreen?.show()
-            }
-            3 -> {
-                println("Названия архивов: ${archives.joinToString(prefix = "[", postfix = "]")}")
-                show()
-            }
-            4 -> exit()
-            else -> {
-                WorkWithUserInput.checkForErrorIfNotCorrectInteger(input)
-                show()
-            }
+        }
+    }
+
+    fun checkArchievesForInput(input: String) {
+        WorkWithUserInput.checkForErrorIfNotCorrectInteger(input)
+        show()
+    }
+
+    fun printArchievesNames() {
+        println("Названия архивов: ${archives.joinToString(prefix = "[", postfix = "]")}")
+    }
+
+    fun goToNewArchiveCreation() {
+        nextScreen = CreateArchiveScreen(archives)
+        nextScreen?.show()
+    }
+
+    fun chooseArchives() {
+        val archiveName = WorkWithUserInput.getUserInput("Введите название архива:")
+        val selectedArchive = archives.find { it.name == archiveName }
+        if (selectedArchive != null) {
+            println("Архив выбран!")
+            nextScreen = SelectNoteScreen(selectedArchive)
+            nextScreen?.previousScreen = this
+            nextScreen?.show()
+        } else {
+            WorkWithUserInput.showError("Архив '$archiveName' не существует.")
+            show()
         }
     }
 }
--- src/main/kotlin/SelectNoteScreen.kt
+++ src/main/kotlin/SelectNoteScreen.kt
@@ -1,42 +1,69 @@
 class SelectNoteScreen(private val archive: Archive) : Screen() {
 
-    private val MENU_WHEN_NOTES_ARE_EMPTY = "Введите\n2. Создать новую заметку\n4. Вернуться в меню Архивов"
+    private val MENU_WHEN_NOTES_ARE_EMPTY = "Введите\n1. Создать новую заметку\n2. Вернуться в меню Архивов"
     private val MENU_WHEN_NOTES_ARE_FILLED = "Введите\n1. Выбрать заметку\n2. Cоздать новую заметку\n3. Посмотреть все заметки\n4. Вернуться в меню Архивов"
 
     override fun show() {
 
         val input = WorkWithUserInput.getUserInput(emptyMenu = MENU_WHEN_NOTES_ARE_EMPTY, filledMenu = MENU_WHEN_NOTES_ARE_FILLED, isEmptyDataSet = archive.notes.isEmpty())
 
-        when (input.toIntOrNull()) {
-            1 -> {
-                val noteName = WorkWithUserInput.getUserInput("Введите название заметки:")
-                val selectedNote = archive.notes.find { it.name == noteName }
-                if (selectedNote != null) {
-                    println("Заметка выбрана!")
-                    nextScreen = NoteScreen(selectedNote)
-                    nextScreen?.previousScreen = this
-                    nextScreen?.show()
-                } else {
-                    WorkWithUserInput.showError("Заметка '$noteName' не существует")
-                    show()
+        if (archive.notes.isEmpty()) {
+            when (input.toIntOrNull()) {
+                1 -> goToNotesCreationMenu()
+                2 -> previousScreen?.show()
+                else -> checkNotesForInput(input)
+            }
+        } else {
+            when (input.toIntOrNull()) {
+                1 -> {
+                    showNotesNames()
+                    chooseNote()
                 }
+                2 -> goToNotesCreationMenu()
+                3 -> showAllNotes()
+                4 -> previousScreen?.show()
+                else -> checkNotesForInput(input)
             }
-            2 -> {
-                nextScreen = CreateNoteScreen(archive)
-                nextScreen?.previousScreen = this
-                nextScreen?.show()
-            }
-            3 -> {
-                val notesString = archive.notes.joinToString("\n", prefix = "[", postfix = "]")
-                println("Заметки:\n$notesString")
-                show()
-            }
-            4 ->
-                previousScreen?.show()
-            else -> {
-                WorkWithUserInput.checkForErrorIfNotCorrectInteger(input)
-                show()
-            }
+        }
+    }
+
+    fun checkNotesForInput(input: String) {
+        WorkWithUserInput.checkForErrorIfNotCorrectInteger(input)
+        show()
+    }
+
+    fun showAllNotes() {
+        val notesString = archive.notes.joinToString("\n", prefix = "[", postfix = "]")
+        println("Заметки:\n$notesString\n")
+        show()
+    }
+
+    fun showNotesNames() {
+        println("Названия заметок:")
+        var startNumber = 1
+        for (i in archive.notes) {
+            println("${startNumber}. ${i.name}")
+            startNumber++
+        }
+    }
+
+    fun goToNotesCreationMenu() {
+        nextScreen = CreateNoteScreen(archive)
+        nextScreen?.previousScreen = this
+        nextScreen?.show()
+    }
+
+    fun chooseNote() {
+        val noteName = WorkWithUserInput.getUserInput("Введите название заметки:")
+        val selectedNote = archive.notes.find { it.name == noteName }
+        if (selectedNote != null) {
+            println("Заметка выбрана!")
+            nextScreen = NoteScreen(selectedNote)
+            nextScreen?.previousScreen = this
+            nextScreen?.show()
+        } else {
+            WorkWithUserInput.showError("Заметка '$noteName' не существует")
+            show()
         }
     }
 }
