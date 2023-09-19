6c254a748841a289c401664532397e5116f0a10d
NieCloud <firdem@yandex.ru>
2023-09-19 08:02:45 +0300

    1. Меню теперь в соответствии с заданием при создании архивов

--- src/main/kotlin/SelectArchiveScreen.kt
+++ src/main/kotlin/SelectArchiveScreen.kt
@@ -1,12 +1,14 @@
 class SelectArchiveScreen(private val archives: MutableList<Archive>) : Screen() {
 
-    private val MENU_WHEN_ARHCHIVES_ARE_EMPTY = "Введите номер:\n1. Создать новый архив\n2. Выйти из программы"
-    private val MENU_WHEN_ARCHIEVES_ARE_FILLED = "Введите номер:\n1. Выбрать архив\n2. Создать новый архив\n3. Посмотреть список всех архивов\n4. Выйти из программы"
 
+    private val MENU_WHEN_ARCHIVES_ARE_EMPTY = "Введите номер:\n1. Создать новый архив\n2. Выйти из программы"
 
     override fun show() {
-
-        val input = WorkWithUserInput.getUserInput(emptyMenu = MENU_WHEN_ARHCHIVES_ARE_EMPTY, filledMenu = MENU_WHEN_ARCHIEVES_ARE_FILLED, isEmptyDataSet = archives.isEmpty())
+        val input = WorkWithUserInput.getUserInput(
+            emptyMenu = MENU_WHEN_ARCHIVES_ARE_EMPTY,
+            filledMenu = getMenuWithArchives(),
+            isEmptyDataSet = archives.isEmpty()
+        )
 
         if (archives.isEmpty()) {
             when (input.toIntOrNull()) {
@@ -15,18 +17,20 @@ class SelectArchiveScreen(private val archives: MutableList<Archive>) : Screen()
                 else -> checkArchievesForInput(input)
             }
         } else {
-            when (input.toIntOrNull()) {
-                1 -> {
-                    printArchievesNames()
-                    chooseArchives()
+            val selectedArchiveIndex = input.toIntOrNull()
+            if (selectedArchiveIndex != null && selectedArchiveIndex >= 3 && selectedArchiveIndex <= archives.size + 2) {
+                // The user selected an archive, so go directly to SelectNoteScreen
+                val selectedArchive = archives[selectedArchiveIndex - 3]
+                println("Архив ${selectedArchive.name} выбран!")
+                val nextScreen = SelectNoteScreen(selectedArchive)
+                nextScreen.previousScreen = this
+                nextScreen.show()
+            } else {
+                when (selectedArchiveIndex) {
+                    1 -> goToNewArchiveCreation()
+                    archives.size + 2 -> exit()
+                    else -> checkArchievesForInput(input)
                 }
-                2 -> goToNewArchiveCreation()
-                3 -> {
-                    printArchievesNames()
-                    show()
-                }
-                4 -> exit()
-                else -> checkArchievesForInput(input)
             }
         }
     }
@@ -45,29 +49,35 @@ class SelectArchiveScreen(private val archives: MutableList<Archive>) : Screen()
         nextScreen?.show()
     }
 
-    fun chooseArchives() {
+    private fun getMenuWithArchives(): String {
+        val menuBuilder = StringBuilder()
+        menuBuilder.append("Введите номер:\n")
+        menuBuilder.append("1. Создать новый архив\n")
+        for ((index, archive) in archives.withIndex()) {
+            menuBuilder.append("${index + 2}. Это мой уже созданный \" ${archive.name}\"\n")
+        }
+        menuBuilder.append("${archives.size + 2}. Выйти из программы")
+        return menuBuilder.toString()
+    }
+
+    private fun chooseArchive() {
         var archiveNumber: Int? = null
 
         while (archiveNumber == null) {
             val input = WorkWithUserInput.getUserInput("Введите номер архива:")
             archiveNumber = input.toIntOrNull()
 
-            if (archiveNumber == null) {
-                WorkWithUserInput.showError("Некорректный ввод. Введите целое число.")
+            if (archiveNumber == null || archiveNumber !in 1..archives.size) {
+                WorkWithUserInput.showError("Некорректный ввод. Введите корректный номер архива.")
             }
         }
 
-
-        val selectedArchive = archives.find { it.uniqueNumber == archiveNumber }
-        if (selectedArchive != null) {
-            println("Архив ${selectedArchive.name} выбран!")
-            nextScreen = SelectNoteScreen(selectedArchive)
-            nextScreen?.previousScreen = this
-            nextScreen?.show()
-        } else {
-            WorkWithUserInput.showError("Архива с номером '$archiveNumber' не существует.")
-            show()
-        }
+        val selectedArchive = archives[archiveNumber - 1]
+        println("Архив ${selectedArchive.name} выбран!")
+        val nextScreen = SelectNoteScreen(selectedArchive)
+        nextScreen.previousScreen = this
+        nextScreen.show()
     }
 }
+
 //
